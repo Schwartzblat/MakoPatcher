136b5cb7fa66a102ba4653ba7e00d60871f8ac0e
alon <alon.ponch@gmail.com>
2026-02-13 16:58:07 +0200

    Add option for paywall and extra arguments

--- main.py
+++ main.py
@@ -15,19 +15,32 @@ def get_args():
                         default='./artifactory.json')
     parser.add_argument('--no-sign', dest='should_sign', help='Whether to sign the output APK', action='store_false',
                         required=False, default=True)
+    parser.add_argument('--extra-artifacts', dest='extra_artifacts',
+                        help='Extra artifact to add to the artifactory, in the format "key:value"',
+                        required=False, default=[], nargs='+')
+    parser.add_argument('--paywall', dest='paywall', help='Whether to add the paywall patch', required=False,
+                        default=None)
     args, _ = parser.parse_known_args()
     return args
 
 
 def main():
     args = get_args()
+    extra_artifacts = {artifact.split(':')[0]: artifact.split(':')[1] for artifact in args.extra_artifacts}
+    external_modules = [
+        ExternalModule(Path(__file__).parent / './smali_generator',
+                       'invoke-static {}, Lcom/smali_generator/TheAmazingPatch;->on_load()V')
+    ]
+    if args.paywall is not None:
+        external_modules.append(ExternalModule(Path(args.paywall),
+                                               'invoke-static {}, Lcom/paywall/Paywall;->on_load()V'))
     with Stitch(
             apk_path=args.apk_path,
             output_apk=args.output,
             temp_path=args.temp_path,
-            external_modules=[ExternalModule(Path('./smali_generator'),
-                                             'invoke-static {}, Lcom/smali_generator/TheAmazingPatch;->on_load()V')],
+            external_modules=external_modules,
             should_sign=args.should_sign,
+            extra_artifacts=extra_artifacts
     ) as stitch:
         stitch.patch()
 
